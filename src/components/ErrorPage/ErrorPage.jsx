d78f6d5c0c41adac8d7c6becc6c41a1c185744be
TheCoderArif <mdarifulislam18539@gmail.com>
2026-01-18 02:19:45 +0600

    Adding initials

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">404! <br /> Not Found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './components/Root/Root.jsx'
+import ErrorPage from './components/ErrorPage/ErrorPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+    {/* <App /> */}
+  </StrictMode>,
+)
